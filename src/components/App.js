1c5f1222b4b4053cd96ae5c3d840ffdb3ef9ad2c
Win-10 <yadavashutosh2193@gmail.com>
2020-11-13 19:26:05 +0530

    List Adding done

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,27 @@ import "./../styles/App.css";
 
 function App() 
 {
+	function AddElement(){
+		const Main = document.getElementById("main");
+		const Data = document.getElementById("task").value;
+		const Text = document.createTextNode(Data);
+		const List = document.createElement("li");
+		List.setAttribute("className", "list");
+		List.appendChild(Text);
+		const Delete = document.createElement("button");
+		Delete.setAttribute("className", "delete");
+		Delete.innerHTML = "delete";
+		List.appendChild(Delete);
+		const Edit = document.createElement("button");
+		Edit.innerHTML = "edit";
+		List.appendChild(Edit);
+		Main.appendChild(List);
+
+	}
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+	   <textarea id = "task"></textarea>
+	   <button id = "btn" onClick = {AddElement}>Add</button>
 	</div>
 	);
 }
