b6c8aa552c324008df74bdce69495fbb9ead544c
Win-10 <yadavashutosh2193@gmail.com>
2020-11-13 21:30:44 +0530

    list done

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,27 @@ import "./../styles/App.css";
 
 function App() 
 {
+	const DeleteItem = () =>{
+		console.log("Delete item");
+	}
 	function AddElement(){
 		const Main = document.getElementById("main");
 		const Data = document.getElementById("task").value;
-		const Text = document.createTextNode(Data);
+		if(Data.length != ""){
+			const Text = document.createTextNode(Data);
 		const List = document.createElement("li");
 		List.setAttribute("className", "list");
 		List.appendChild(Text);
 		const Delete = document.createElement("button");
 		Delete.setAttribute("className", "delete");
+		Delete.setAttribute("onClick", {DeleteItem});
 		Delete.innerHTML = "delete";
 		List.appendChild(Delete);
 		const Edit = document.createElement("button");
 		Edit.innerHTML = "edit";
 		List.appendChild(Edit);
 		Main.appendChild(List);
+		console.log(List);}
 
 	}
 	return (
