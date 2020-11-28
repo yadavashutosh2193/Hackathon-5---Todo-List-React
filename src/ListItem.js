cf8b82ef09d4bf7101101f3ea4e8ff9440ef9d64
Win-10 <yadavashutosh2193@gmail.com>
2020-11-28 23:53:24 +0530

    done todo

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 function ListItem(props){
+    const [editedItem, setEditedItem] = useState(props.Item);
+    const [Edit, setEdit] = useState(false);
+    
+    const SaveTask = () =>{
+        props.EditHandler(editedItem, props.idx);
+        setEdit(false);
+    }
     return (<>
-        <p className = "list">{props.Item}</p>
-        <button className = "edit">Edit</button>
-        <button className = "delete">Delete</button>
-        <input type = "text" placeholder = "Edit Text" className = "editTask"/>
-        <button className = "saveTask">Save</button>
+    {!Edit ? <><p className = "list">{props.Item}</p>
+        <button className = "edit" onClick = {()=> setEdit(true)}>Edit</button>
+        <button className = "delete" onClick = {()=>props.DeleteItem(props.idx)}>Delete</button></> : 
+        <>
+        <input type = "text" value = {editedItem} placeholder = "Edit Text" className = "editTask" onChange = {(e)=>setEditedItem(e.target.value)}/>
+        <button className = "saveTask" disabled = {editedItem.length === 0} onClick = {SaveTask}>Save</button>
+        </>}
+        
+        
     </>)
 }
 export default ListItem
--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,21 @@ const AddItemToList = () => {
 	})
 	setItem("");
 };	
+const Delete = (index)=>{
+	 items.splice(index, 1);
+	 setItems([...items]);
+}
+const SaveNewItem = ( EditedItem, index) =>{
+	items[index] = EditedItem;
+	setItems([...items]);
+}
 	return (
 	<div id="main">
 	   <textarea id = "task" value = {item} placeholder= "Enter Item name" onChange = {(e)=>setItem(e.target.value)}></textarea>
-	   <button id = "btn" onClick = {AddItemToList}>Add</button>
+	   <button id = "btn" onClick = {AddItemToList} disabled = {item.trim().length === 0}>Add</button>
 	   {items.map((newItem, Idx)=>{
-		   return <ListItem key = {Idx} Item = {newItem}/>
+		   return <ListItem key = {`${newItem}_${Idx}`} idx = {Idx} 
+		   Item = {newItem} DeleteItem = {Delete} EditHandler = {SaveNewItem}/>
 	   })}
 	   
 	</div>
